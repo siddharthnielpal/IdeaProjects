76f3fe4daa6f527bf75652b20d614015a025b73f
Siddharth Pal <siddharth@Tornado.local>
2016-10-20 23:04:44 -0700

    Cosmetic change.

--- Quadratic/src/Main.java
+++ Quadratic/src/Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-	    System.out.print("a= ");
+        System.out.print("a= ");
         int a = scanner.nextInt();
 
         System.out.print("b= ");
